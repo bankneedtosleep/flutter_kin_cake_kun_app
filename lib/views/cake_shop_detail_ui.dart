1ef3f3d9824b557f702f18b41e0d8d80770a1d48
bankneedtosleep <bank03124@gmail.com>
2026-03-12 14:49:47 +0700

    finish

--- lib/views/cake_shop_detail_ui.dart
+++ lib/views/cake_shop_detail_ui.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_kin_cake_kun_app/models/cake_shop.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class CakeShopDetailUi extends StatefulWidget {
   final CakeShop? cakeShop;
@@ -11,6 +15,30 @@ class CakeShopDetailUi extends StatefulWidget {
 }
 
 class _CakeShopDetailUiState extends State<CakeShopDetailUi> {
+  // เมธอดโทร
+  Future<void> _makePhoneCall(String phoneNumber) async {
+    final Uri phoneUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+
+    if (await canLaunchUrl(phoneUri)) {
+      await launchUrl(phoneUri);
+    } else {
+      throw 'ไม่สามารถโทรไปที่ $phoneNumber ได้';
+    }
+  }
+
+  // เมธอดเปิด Browser (-> web, facebook, google map)
+  Future<void> _launchInBrowser(Uri url) async {
+    if (!await launchUrl(
+      url,
+      mode: LaunchMode.externalApplication,
+    )) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +56,254 @@ class _CakeShopDetailUiState extends State<CakeShopDetailUi> {
           icon: const Icon(
             Icons.arrow_back_ios_new,
             color: Colors.white,
+          ),
+        ),
+      ),
+
+      // ส่วนของแสดงรายละเอียดร้าน
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Center(
+            child: Column(
+              children: [
+                // รูปร้านหลัก
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.asset(
+                    'assets/images/${widget.cakeShop?.image1}',
+                    width: 120,
+                    height: 85,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.asset(
+                        'assets/images/${widget.cakeShop?.image2}',
+                        width: 120,
+                        height: 85,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.asset(
+                        'assets/images/${widget.cakeShop?.image3}',
+                        width: 120,
+                        height: 85,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 20),
+
+                // ชื่อร้าน
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    widget.cakeShop!.name!,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red[400],
+                    ),
+                  ),
+                ),
+
+                // รายละเอียดร้าน
+                Text(
+                  widget.cakeShop!.description!,
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey[800],
+                  ),
+                ),
+
+                const SizedBox(height: 15),
+
+                // เวลาเปิดปิด
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'เวลาเปิด-ปิด ⌚',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red[400],
+                    ),
+                  ),
+                ),
+
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    widget.cakeShop!.openCloseTime!,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 15),
+
+                // ที่ตั้งร้าน
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'ที่ตั้งร้าน 📍',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red[400],
+                    ),
+                  ),
+                ),
+
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    widget.cakeShop!.address!,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+
+                // ปุ่มโทร
+                ElevatedButton(
+                  onPressed: () {
+                    _makePhoneCall(widget.cakeShop!.phone!);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  child: Text(
+                    '📞 ${widget.cakeShop!.phone!}',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                // เว็บไซต์ร้าน
+                ListTile(
+                  onTap: () {
+                    _launchInBrowser(
+                      Uri.parse(widget.cakeShop!.website!),
+                    );
+                  },
+                  leading: Icon(FontAwesomeIcons.globe,
+                      color: Colors.orange[400], size: 40),
+                  title: Text(
+                    widget.cakeShop!.website!,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.orange[800],
+                    ),
+                  ),
+                  trailing: Icon(
+                    FontAwesomeIcons.link,
+                    color: Colors.orange[400],
+                    size: 20,
+                  ),
+                ),
+                SizedBox(height: 25),
+                // Facebook ร้าน
+                InkWell(
+                  onTap: () {
+                    _launchInBrowser(
+                      Uri.parse(widget.cakeShop!.facebook!),
+                    );
+                  },
+                  child: Icon(
+                    FontAwesomeIcons.facebook,
+                    color: Colors.blue[800],
+                    size: 40,
+                  ),
+                ),
+                SizedBox(height: 20),
+                // แผนที่
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 300.0,
+                  child: FlutterMap(
+                    //กำหนดตำแหน่งของแผนที่
+                    options: MapOptions(
+                      initialCenter: LatLng(
+                        double.parse(widget.cakeShop!.latitude!),
+                        double.parse(widget.cakeShop!.longitude!),
+                      ),
+                      initialZoom: 15.0,
+                    ),
+                    //วาดแผนที่
+                    children: [
+                      TileLayer(
+                        urlTemplate:
+                            'https://{s}.google.com/vt/lyrs=m,h&x={x}&y={y}&z={z}&hl=ar-MA&gl=MA',
+                        subdomains: const ['mt0', 'mt1', 'mt2', 'mt3'],
+                        userAgentPackageName: 'com.example.app',
+                      ),
+                      RichAttributionWidget(
+                        attributions: [
+                          TextSourceAttribution(
+                            'OpenStreetMap contributors',
+                            onTap: () {
+                              launchUrl(
+                                Uri.parse(
+                                    'https://openstreetmap.org/copyright'),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                      // กำหนด Marker
+                      MarkerLayer(
+                        markers: [
+                          Marker(
+                            point: LatLng(
+                              double.parse(widget.cakeShop!.latitude!),
+                              double.parse(widget.cakeShop!.longitude!),
+                            ),
+                            child: InkWell(
+                              onTap: () {
+                                //เปิด Google Maps ด้วยพิกัดที่กำหนด
+                                String googleMapsUrl =
+                                    'https://www.google.com/maps/search/?api=1&query=${widget.cakeShop!.latitude!},${widget.cakeShop!.longitude!}';
+                                _launchInBrowser(Uri.parse(googleMapsUrl));
+                              },
+                              child: Icon(
+                                // FontAwesomeIcons.locationArrow,
+                                Icons.location_pin,
+                                color: Colors.red,
+                                size: 40.0,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
